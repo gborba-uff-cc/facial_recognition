e07980d64ff0be6b884ff58c6be4eaa9150d10bb
Gabriel Borba <gborba@id.uff.br>
2023-04-15 14:11:25 -0300

    adding the camera_view screen on the app router

--- lib/main.dart
+++ lib/main.dart
@@ -1,36 +1,50 @@
+import 'package:facial_recognition/screens/camera_view/camera_view.dart';
 import 'package:facial_recognition/screens/placeholder/placeholder_screen.dart';
+import 'package:facial_recognition/utils/project_logger.dart';
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
+import 'package:camera/camera.dart';
 
-void main() {
-  runApp(const MainApp());
+void main() async {
+  // hide async code with a splash screen
+  runApp(const Placeholder());
+  // start async code as soon as possible
+  final futures = await Future.wait(
+    [
+      availableCameras(),
+    ],
+  );
+  final availableCams = futures[0];
+  // sync code goes under here
+  projectLogger.fine(availableCams);
+  //
+  runApp(MainApp(cameras: availableCams));
 }
 
 class MainApp extends StatelessWidget {
-  const MainApp({super.key});
+  final List<CameraDescription> cameras;
+
+  const MainApp({super.key, required this.cameras});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp.router(
-      theme: _theme,
-      routerConfig: _router,
+      theme: ThemeData(useMaterial3: true),
+      routerConfig: GoRouter(
+        routes: [
+          GoRoute(
+            path: '/',
+            builder: (context, state) => const PlaceholderScreen(
+                displayText: 'Tela principal', nextScreen: '/camera_view'),
+            routes: <RouteBase>[
+              GoRoute(
+                path: 'camera_view',
+                builder: (context, state) => CameraView(cameras: cameras),
+              ),
+            ],
+          ),
+        ],
+      ),
     );
   }
 }
-
-final _theme = ThemeData(useMaterial3: true);
-
-final _router = GoRouter(
-  routes: [
-    GoRoute(
-      path: '/',
-      builder: (context, state) => const PlaceholderScreen(displayText: 'Tela principal', nextScreen: '/face_page'),
-      routes: <RouteBase>[
-        GoRoute(
-          path: 'face_page',
-          builder: (context, state) => const PlaceholderScreen(displayText: 'Tela filha', nextScreen: ''),
-        ),
-      ],
-    ),
-  ],
-);
