34264cd598a3411729ea266064ff7b32d9811486
Gabriel Borba <gborba@id.uff.br>
2023-05-02 22:40:39 -0300

    converting yuv420 to rgb and creating an image that can be handled

--- lib/domain.dart
+++ lib/domain.dart
@@ -1,9 +1,11 @@
+import 'dart:typed_data';
 import 'dart:ui';
 
 import 'package:camera/camera.dart';
 import 'package:facial_recognition/utils/project_logger.dart';
 import 'package:flutter/foundation.dart';
 import 'package:google_mlkit_face_detection/google_mlkit_face_detection.dart';
+import 'package:image/image.dart' as pkg_image;
 
 /// main use case for the app
 // // FIXME - provide the missing components
@@ -67,6 +69,7 @@ Future<List<Face>> detectFaces(InputImage image) {
 // }
 
 // HELPER ------
+/// Convert a [image] from camera to an image used by the Google ML Kit
 InputImage? toInputImage(CameraImage image, CameraController controller) {
   final imageRotation = InputImageRotationValue.fromRawValue(
       controller.description.sensorOrientation);
@@ -112,4 +115,57 @@ InputImage? toInputImage(CameraImage image, CameraController controller) {
       InputImage.fromBytes(bytes: bytes, inputImageData: inputImageData);
 
   return inputImage;
+}
+
+pkg_image.Image toLogicalImage({
+  required int width,
+  required int height,
+  required ByteBuffer rgbBytes
+}) {
+  final image = pkg_image.Image.fromBytes(
+    width: width,
+    height: height,
+    bytes: rgbBytes,
+    order: pkg_image.ChannelOrder.rgb,
+  );
+
+  return image;
+}
+
+/// Convert YCbCr (called YUV) 4:2:0 3-plane to an RGB 1-plane.
+ByteBuffer yCbCr420ToRgb({
+  required final int width,
+  required final int height,
+  required List<Plane> planes
+}) {
+  final yBytes = planes[0].bytes;   // Y
+  final cbBytes = planes[1].bytes;  // U
+  final crBytes = planes[2].bytes;  // V
+  final yBytesPerPixel = planes[0].bytesPerPixel ?? 1;
+  final yBytesPerRow = planes[0].bytesPerRow;
+  final cbCrBytesPerPixel = planes[1].bytesPerPixel ?? 1;
+  final cbCrBytesPerRow = planes[1].bytesPerRow;
+
+  final WriteBuffer rgbBytes = WriteBuffer(startCapacity: 3*width*height);
+
+  for (int x = 0; x < width; x++) {
+    for (int y = 0; y < height; y++) {
+      final int yIndex = x~/2 * yBytesPerPixel + y~/2 * yBytesPerRow;
+      final int cbCrIndex = x~/2 * cbCrBytesPerPixel + (y~/2 * cbCrBytesPerRow);
+
+      final yp = yBytes[yIndex];
+      final up = cbBytes[cbCrIndex];
+      final vp = crBytes[cbCrIndex];
+      final r = (yp + vp * 1436 / 1024 - 179).round().clamp(0, 255);
+      final g = (yp - up * 46549 / 131072 + 44 - vp * 93604 / 131072 + 91)
+          .round()
+          .clamp(0, 255);
+      final b = (yp + up * 1814 / 1024 - 227).round().clamp(0, 255);
+      rgbBytes.putUint8(r);
+      rgbBytes.putUint8(g);
+      rgbBytes.putUint8(b);
+    }
+  }
+
+  return rgbBytes.done().buffer;
 }
