51bf03bd03b08334b4adf6c23226fed57f23016a
Gabriel Borba <gborba@id.uff.br>
2024-08-05 22:39:15 -0300

    updated the camera identification screen to use the wrapped camera widget

--- lib/screens/camera_identification_screen.dart
+++ lib/screens/camera_identification_screen.dart
@@ -1,6 +1,5 @@
-import 'dart:async';
-
 import 'package:camera/camera.dart';
+import 'package:facial_recognition/screens/widgets/camera_wrapper.dart';
 import 'package:facial_recognition/use_case/camera_identification.dart';
 import 'package:facial_recognition/utils/project_logger.dart';
 import 'package:flutter/foundation.dart';
@@ -21,239 +20,84 @@ class CameraIdentificationScreen extends StatefulWidget {
 }
 
 class _CameraIdentificationScreenState extends State<CameraIdentificationScreen> with WidgetsBindingObserver {
-  late CameraDescription _selectedCamera;
-  // NOTE - a new controller is needed after a dispose (dispose can be called
-  // more than once), so...
-  //  create controller on: initState, app is resumed, change camera source
-  //  delete controller on: dispose, app is inactive, change camera source
-  CameraController? cameraController;
   final List<Uint8List> facesPhotos = [];
-  // controll the frequency and which what images are processed
-  final StreamController<CameraImage> _cameraImageStreamController = StreamController();
-  StreamSubscription<CameraImage>? _cameraImageStreamSubscription;
-  final _imageCounterFilter = _ModularCounter(30);
+  bool _isAutoMode = false;
 
   @override
   void initState() {
     super.initState();
-    // Register this object as a binding observer to receive application events
-    WidgetsBinding.instance.addObserver(this);
-    // update useCase.showFaceImages callback with how to show detected faces
-    widget.useCase.showFaceImages = (jpegImages) {
+    widget.useCase.showFaceImages =  (jpegImages) {
       if (mounted) {
         setState(() {
           facesPhotos.addAll(jpegImages);
         });
       }
     };
-    // controller life cycle is handled here and on didChangeAppLifecycleState
-    if (widget.cameras.isNotEmpty) {
-      _selectedCamera = widget.cameras[1];
-      projectLogger.fine(_selectedCamera);
-      _startCameraController(_selectedCamera).then((value) {
-        if (mounted && value != null) {
-          setState(() {
-            cameraController = value;
-          });
-        }
-      });
-    }
-    // tie together the 'camera images acquirement' to the 'image processing'
-    _cameraImageStreamSubscription = _cameraImageStreamController.stream
-        .where((cameraImage) {
-          final isZero = _imageCounterFilter.current == 0;
-          _imageCounterFilter.tick();
-          return isZero ? true : false;
-        })
-        .listen((cameraImage) {
-      widget.useCase.onNewCameraImage(
-        cameraImage,
-        cameraController!.description.sensorOrientation,
-      );
-    });
   }
 
   @override
   void dispose() {
-    _cameraImageStreamSubscription?.cancel();
-    _cameraImageStreamController.close();
-    _disposeCameraController(cameraController);
-    cameraController = null;
-    WidgetsBinding.instance.removeObserver(this);
     super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
-    final controller = cameraController;
-
-    Widget body;
-    if (controller == null) {
-      body = const Center(
-        child: Text('Iniciando a camera'),
-      );
-    }
-    else {
-      body = Column(
-        children: [
-          CameraPreview(
-            controller,
-            child: const Icon(
-              Icons.arrow_upward,
-            ),
-          ),
-          Expanded(
-            child: ListView.builder(
-              itemCount: facesPhotos.length,
-              itemBuilder: (context, index) => Image.memory(facesPhotos[index]),
-              scrollDirection: Axis.horizontal,
-            ),
-          )
-        ],
-      );
-    }
-
+    final cameraWidget = CameraWrapper(
+      camerasAvailable: widget.cameras,
+      imageCaptureHandler: _isAutoMode
+          ? null
+          : (cameraDescription, cameraImage) => widget.useCase.onNewCameraImage(
+              cameraImage, cameraDescription.sensorOrientation),
+      imageStreamHandler: !_isAutoMode
+          ? null
+          : (cameraDescription, cameraImage) => widget.useCase.onNewCameraImage(
+              cameraImage, cameraDescription.sensorOrientation),
+    );
     return Scaffold(
       appBar: AppBar(),
-      body: body,
+      body: SizedBox.expand(
+        child: Column(
+          children: [
+            Stack(
+              children: [
+                cameraWidget,
+                Positioned.fill(
+                  child: Align(
+                    alignment: Alignment.bottomLeft,
+                    child: Padding(
+                      padding: EdgeInsets.all(cameraWidget.basePadding),
+                      child: SizedBox(
+                        width: cameraWidget.baseIconSize,
+                        height: cameraWidget.baseIconSize,
+                        child: Center(
+                          child: _isAutoMode
+                              ? FilledButton(
+                                  onPressed: () =>
+                                      setState(() => _isAutoMode = !_isAutoMode),
+                                  child: const Text('Auto'),
+                                )
+                              : FilledButton.tonal(
+                                  onPressed: () =>
+                                      setState(() => _isAutoMode = !_isAutoMode),
+                                  child: const Text('Auto'),
+                                ),
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+              ] ,
+            ),
+            Expanded(
+              child: ListView.builder(
+                itemCount: facesPhotos.length,
+                itemBuilder: (context, index) => Image.memory(facesPhotos[index]),
+                scrollDirection: Axis.horizontal,
+              ),
+            )
+          ],
+        ),
+      ),
     );
-  }
-
-  void _disposeCameraController(CameraController? controller) async {
-    if (controller == null) {
-      return;
-    }
-
-    if (controller.value.isStreamingImages) {
-      await controller.stopImageStream();
-    }
-    await controller.dispose();
-    projectLogger.fine('camera controller disposed.');
-  }
-
-  Future<CameraController?> _startCameraController(CameraDescription description) async {
-    final controller = CameraController(
-      description,
-      // FIXME - as per documentation resolution limited due streaming and
-      // running the preview widget
-      ResolutionPreset.low,
-      // NOTE - let it fallback to platform's default to be able to stream
-      imageFormatGroup: null,
-      enableAudio: false,
-    );
-
-    try {
-      await controller.initialize();
-      // create a stream of camera images
-      await controller.startImageStream(
-        (image) => _cameraImageStreamController.add(image),
-      );
-      return controller;
-    }
-    on CameraException catch (e) {
-      projectLogger.severe('cameraController could not be initialized');
-      cameraController = null;
-
-      switch (e.code) {
-        case 'CameraAccessDenied':
-          projectLogger.severe('You have denied camera access.');
-          break;
-        // iOS only
-        case 'CameraAccessDeniedWithoutPrompt':
-          projectLogger
-              .severe('Please go to Settings app to enable camera access.');
-          break;
-        // iOS only
-        case 'CameraAccessRestricted':
-          projectLogger.severe('Camera access is restricted.');
-          break;
-        case 'AudioAccessDenied':
-          projectLogger.severe('You have denied audio access.');
-          break;
-        // iOS only
-        case 'AudioAccessDeniedWithoutPrompt':
-          projectLogger
-              .severe('Please go to Settings app to enable audio access.');
-          break;
-        // iOS only
-        case 'AudioAccessRestricted':
-          projectLogger.severe('Audio access is restricted.');
-          break;
-        default:
-          projectLogger.shout('Unknow CameraException code', e);
-      }
-    }
-    return null;
-  }
-
-  @override
-  void didChangeAppLifecycleState(AppLifecycleState state) {
-    super.didChangeAppLifecycleState(state);
-
-    // LINK - https://docs.flutter.dev/get-started/flutter-for/android-devs#how-do-i-listen-to-android-activity-lifecycle-events
-    // LINK - https://api.flutter.dev/flutter/dart-ui/AppLifecycleState.html
-    projectLogger.fine('#camera_view #app_changed_state #$state');
-
-    // SECTION - manage camera resources.
-    final controller = cameraController;
-
-    // // nothing to manage or controller not initialized
-    // if (controller == null || !controller.value.isInitialized) {
-    //   return;
-    // }
-
-    switch (state) {
-      // start the camera controller
-      case AppLifecycleState.resumed: // visible and focused
-        _startCameraController(_selectedCamera).then((value) {
-          projectLogger.fine('camera controller $value');
-          if (mounted && value != null) {
-            setState(() {
-              cameraController = value;
-            });
-          }
-        });
-        break;
-      // stop the camera controller
-      // visible (or obscured by a system view) and not focused
-      case AppLifecycleState.inactive:
-      // not visible and not focused
-      case AppLifecycleState.paused:
-      //
-      case AppLifecycleState.detached:
-        _disposeCameraController(controller);
-        cameraController = null;
-        break;
-      default:
-        null;
-    }
-    // !SECTION
-  }
-
-  // when there is a change on cameraController.value
-  void onCameraControllerValueChange() {
-    projectLogger.fine('#camera_controller #value_changed ${cameraController?.value}');
-    final controller = cameraController;
-
-    if (mounted) {
-      setState(() {});
-    }
-    if (controller != null && controller.value.hasError) {
-      projectLogger
-          .severe('Camera error: ${controller.value.errorDescription}');
-    }
-  }
-}
-
-class _ModularCounter {
-  _ModularCounter(this.module);
-
-  final int module;
-  int _current = 0;
-
-  int get current => _current;
-
-  void tick() {
-    _current = (_current+1) % module;
   }
 }
