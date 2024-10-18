47842460195d300981f604939e63465af85380e9
Gabriel Borba <gborba@id.uff.br>
2024-10-18 04:21:09 -0300

    w.i.p. attendance summary screen revamp

--- lib/screens/attendance_summary_screen.dart
+++ lib/screens/attendance_summary_screen.dart
@@ -1,114 +1,188 @@
 import 'package:facial_recognition/models/domain.dart';
+import 'package:facial_recognition/screens/common/app_defaults.dart';
 import 'package:facial_recognition/use_case/attendance_summary.dart';
+import 'package:facial_recognition/utils/ui.dart';
 import 'package:flutter/material.dart';
 
 class AttendanceSummaryScreen extends StatelessWidget {
-  factory AttendanceSummaryScreen({
-    Key? key,
-    required AttendanceSummary useCase,
-  }) =>
-      AttendanceSummaryScreen._private(
-        key: key,
-        useCase: useCase,
-        summary: useCase.getSubjectClassAttendance(),
-        facePicture: useCase.getStudentFaceImage(),
-      );
-
-  const AttendanceSummaryScreen._private({
+  const AttendanceSummaryScreen({
     super.key,
     required this.useCase,
-    required Map<Student, List<Attendance>> summary,
-    required Map<Student, FacePicture?> facePicture,
-  })  : _summary = summary,
-        _facePicture = facePicture;
+  });
 
   final AttendanceSummary useCase;
-  final Map<Student, List<Attendance>> _summary;
-  final Map<Student, FacePicture?> _facePicture;
 
   @override
   Widget build(BuildContext context) {
-    final summary = _summary.entries;
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(
-          'Presenças',
-          maxLines: 1,
-          style: Theme.of(context).textTheme.headlineLarge,
-          overflow: TextOverflow.fade,
-        ),
-      ),
-      body: ConstrainedBox(
-        constraints: const BoxConstraints(
-          minWidth: 300,
-          maxWidth: 600,
-        ),
-        child: summary.isEmpty
-            ? const Center(
-                child: Text('Não há alunos na turma'),
-              )
-            : Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: ListView.builder(
-                  itemCount: summary.length,
-                  itemBuilder: (context, index) {
-                    final element = summary.elementAt(index);
-                    final picture = _facePicture[element.key];
-                    final leadingWidget = picture == null ? const Icon(Icons.person) : Image.memory(picture.faceJpeg, fit: BoxFit.cover,);
-                    final textTheme = Theme.of(context).textTheme;
-                    const cardHeight = 100.0;
-                    return Padding(
-                      padding: const EdgeInsets.only(bottom: 4.0),
-                      child: ConstrainedBox(
-                        constraints: BoxConstraints.loose(
-                          const Size.fromHeight(cardHeight),
-                        ),
-                        child: CustomTile(
-                          leading: AspectRatio(
-                            aspectRatio: 1,
-                            child: leadingWidget,
-                          ),
-                          title: Padding(
-                            padding: const EdgeInsets.fromLTRB(8.0,4.0,8.0,0.0),
-                            child: Align(
-                              alignment: Alignment.centerLeft,
-                              child: Text(
-                                element.key.individual.displayFullName,
-                                style: textTheme.titleMedium,
-                              ),
-                            ),
-                          ),
-                          subtitle: Padding(
-                            padding: const EdgeInsets.fromLTRB(8.0,0.0,8.0,4.0),
-                            child: Align(
-                              alignment: Alignment.centerLeft,
-                              child: Text(
-                                element.key.registration,
-                              ),
-                            ),
-                          ),
-                          trailing: SizedBox(
-                            width: 60.0,
-                            height: cardHeight,
-                            child: Column(
-                              mainAxisAlignment: MainAxisAlignment.center,
-                              children: [
-                                Text(
-                                  element.value.length.toString(),
-                                  style: textTheme.headlineSmall,
-                                  maxLines: 1,
-                                  overflow: TextOverflow.clip,
-                                  textAlign: TextAlign.center,
-                                ),
-                              ],
-                            ),
-                          ),
-                        ),
-                      ),
-                    );
-                  },
-                ),
+    final classAttendance = useCase.classAttendance.entries;
+    final nRegisteredLessons = useCase.nRegisteredLessons;
+    final nPastLessons = useCase.nPastLessons;
+    final lastLesson = useCase.lastLesson;
+    final nAbsentsLastLesson = useCase.nAbsentsLastLesson;
+    final pictureOfFaces = useCase.studentsFaceImage;
+    return AppDefaultMenuScaffold(
+      appBar: AppDefaultAppBar(title: 'Presenças'),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Padding(
+            padding: const EdgeInsets.only(bottom: 8.0),
+            child: Text(
+              'Resumo',
+              style: Theme.of(context).textTheme.titleLarge,
             ),
+          ),
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Text(
+                'Ausências',
+                style: Theme.of(context).textTheme.titleMedium,
+              ),
+              Padding(
+                padding: const EdgeInsets.only(left: 16.0),
+                child: Text(
+                  '$nAbsentsLastLesson aluno${nAbsentsLastLesson == 1 ? '' : 's'}(a${nAbsentsLastLesson == 1 ? '' : 's'}) não ${nAbsentsLastLesson == 1 ? 'compareceu' : 'compareceram'} na última aula',
+                  style: Theme.of(context).textTheme.titleSmall,
+                ),
+              ),
+            ],
+          ),
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Text(
+                'Última aula',
+                style: Theme.of(context).textTheme.titleMedium,
+              ),
+              Padding(
+                padding: const EdgeInsets.only(left: 16.0),
+                child: Text(
+                  lastLesson == null
+                      ? 'Nenhuma aula ministrada'
+                      :  'Aula em ${dateTimeToString(lastLesson.utcDateTime.toLocal())}',
+                  style: Theme.of(context).textTheme.titleSmall,
+                ),
+              ),
+            ],
+          ),
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Text(
+                'Aulas até agora',
+                style: Theme.of(context).textTheme.titleMedium,
+              ),
+              Padding(
+                padding: const EdgeInsets.only(left: 16.0),
+                child: Text(
+                  '$nPastLessons aula${nPastLessons == 1 ? '' : 's'} ${nPastLessons == 1 ? 'foi' : 'foram'} ministrada${nPastLessons == 1 ? '' : 's'}',
+                  style: Theme.of(context).textTheme.titleSmall,
+                ),
+              ),
+            ],
+          ),
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Text(
+                'Aulas cadastradas',
+                style: Theme.of(context).textTheme.titleMedium,
+              ),
+              Padding(
+                padding: const EdgeInsets.only(left: 16.0),
+                child: Text(
+                  '$nRegisteredLessons aula${nRegisteredLessons == 1 ? '' : 's'} ${nRegisteredLessons == 1 ? 'foi' : 'foram'} cadastrada${nRegisteredLessons == 1 ? '' : 's'}',
+                  style: Theme.of(context).textTheme.titleSmall,
+                ),
+              ),
+            ],
+          ),
+          Align(
+            alignment: Alignment.centerLeft,
+            child: Text(
+              'Acompanhamento',
+              style: Theme.of(context).textTheme.titleLarge,
+            ),
+          ),
+          Flexible(
+            fit: FlexFit.tight,
+            child: classAttendance.isEmpty
+                ? const Center(
+                    child: Text('Não há alunos na turma'),
+                  )
+                : ListView.builder(
+                    itemCount: classAttendance.length,
+                    itemBuilder: (context, index) {
+                      final element = classAttendance.elementAt(index);
+                      final picture = pictureOfFaces[element.key];
+                      final leadingWidget = picture == null
+                          ? const Icon(Icons.person)
+                          : Image.memory(
+                              picture.faceJpeg,
+                              fit: BoxFit.cover,
+                            );
+                      final textTheme = Theme.of(context).textTheme;
+                      const cardHeight = 100.0;
+                      return Padding(
+                        padding: const EdgeInsets.only(bottom: 8.0),
+                        child: ConstrainedBox(
+                          constraints: BoxConstraints.loose(
+                            const Size.fromHeight(cardHeight),
+                          ),
+                          child: CustomTile(
+                            leading: AspectRatio(
+                              aspectRatio: 1,
+                              child: leadingWidget,
+                            ),
+                            title: Padding(
+                              padding:
+                                  const EdgeInsets.fromLTRB(8.0, 4.0, 8.0, 0.0),
+                              child: Align(
+                                alignment: Alignment.centerLeft,
+                                child: Text(
+                                  element.key.individual.displayFullName,
+                                  style: textTheme.titleMedium,
+                                ),
+                              ),
+                            ),
+                            subtitle: Padding(
+                              padding:
+                                  const EdgeInsets.fromLTRB(8.0, 0.0, 8.0, 4.0),
+                              child: Align(
+                                alignment: Alignment.centerLeft,
+                                child: Text(
+                                  element.key.registration,
+                                ),
+                              ),
+                            ),
+                            trailing: SizedBox(
+                              width: 60.0,
+                              height: cardHeight,
+                              child: Column(
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                children: [
+                                  Text(
+                                    element.value.length.toString(),
+                                    style: textTheme.headlineSmall,
+                                    maxLines: 1,
+                                    overflow: TextOverflow.clip,
+                                    textAlign: TextAlign.center,
+                                  ),
+                                ],
+                              ),
+                            ),
+                          ),
+                        ),
+                      );
+                    },
+                  ),
+          ),
+        ],
       ),
     );
   }
--- lib/use_case/attendance_summary.dart
+++ lib/use_case/attendance_summary.dart
@@ -2,22 +2,125 @@ import 'package:facial_recognition/interfaces.dart';
 import 'package:facial_recognition/models/domain.dart';
 
 class AttendanceSummary {
-  AttendanceSummary(
-    this.domainRepository,
-    this.subjectClass,
-  );
+  factory AttendanceSummary(
+    IDomainRepository domainRepository,
+    SubjectClass subjectClass,
+  ) {
+    final theAttendances = domainRepository
+        .getSubjectClassAttendance([subjectClass])[subjectClass]!;
+    final attendances = Map<Student, List<Attendance>>.unmodifiable(
+      theAttendances.map<Student, List<Attendance>>(
+        (key, value) => MapEntry<Student, List<Attendance>>(
+          key,
+          List<Attendance>.unmodifiable(
+            value.toList()..sort(_sortAttendancesByDateTime),
+          ),
+        ),
+      ),
+    );
+    final theLessons = domainRepository
+          .getLessonFromSubjectClass([subjectClass])[subjectClass]!;
+    final lessons = List<Lesson>.unmodifiable(
+      theLessons.toList()..sort(_sortLessonsByDateTime),
+    );
+    final now = DateTime.now().toUtc();
+    final pastLessons = List<Lesson>.unmodifiable(
+      lessons.where(
+        (element) => element.utcDateTime.compareTo(now) < 0,
+      ),
+    );
+    final lastLesson = pastLessons.lastOrNull;
+    final absentsLastLesson = List<Student>.unmodifiable(
+      attendances.entries.where(
+        (entry) {
+          final entryValue = entry.value;
+          if (lastLesson == null) {
+            return false;
+          }
+          if (entryValue.isEmpty ||
+              entryValue.last.lesson.utcDateTime
+                      .compareTo(lastLesson.utcDateTime) <
+                  0) {
+            return true;
+          }
+          return false;
+        },
+      ).map(
+        (entry) => entry.key,
+      ),
+    );
+    final classStudents = domainRepository
+        .getStudentFromSubjectClass([subjectClass])[subjectClass]!;
+    final studentsFaceImage = Map<Student, FacePicture?>.unmodifiable(domainRepository.getFacePictureFromStudent(classStudents));
 
-  final IDomainRepository domainRepository;
-  final SubjectClass subjectClass;
+    return AttendanceSummary._private(
+      domainRepository: domainRepository,
+      subjectClass: subjectClass,
+      attendances: attendances,
+      lessons: lessons,
+      now: now,
+      pastLessons: pastLessons,
+      lastLesson: lastLesson,
+      absentsLastLesson: absentsLastLesson,
+      studentsFaceImage: studentsFaceImage,
+    );
+  }
+
+  AttendanceSummary._private({
+    required IDomainRepository domainRepository,
+    required SubjectClass subjectClass,
+    required Map<Student, List<Attendance>> attendances,
+    required List<Lesson> lessons,
+    required DateTime now,
+    required List<Lesson> pastLessons,
+    required Lesson? lastLesson,
+    required List<Student> absentsLastLesson,
+    required Map<Student, FacePicture?> studentsFaceImage,
+  })  :
+        _domainRepository = domainRepository,
+        _subjectClass = subjectClass,
+        _attendances = attendances,
+        _lessons = lessons,
+        _now = now,
+        _pastLessons = pastLessons,
+        _lastLesson = lastLesson,
+        _absentsLastLesson = absentsLastLesson,
+        _studentsFaceImage = studentsFaceImage;
+
+  final IDomainRepository _domainRepository;
+  final SubjectClass _subjectClass;
+  final Map<Student, List<Attendance>> _attendances;
+  final List<Lesson> _lessons;
+  final DateTime _now;
+  final List<Lesson> _pastLessons;
+  final Lesson? _lastLesson;
+  final List<Student> _absentsLastLesson;
+  final Map<Student, FacePicture?> _studentsFaceImage;
+
+  int get nRegisteredLessons => _lessons.length;
+  int get nPastLessons => _pastLessons.length;
+  Lesson? get lastLesson => _lastLesson;
+  int get nAbsentsLastLesson => _absentsLastLesson.length;
+  Map<Student, List<Attendance>> get classAttendance => _attendances;
+  Map<Student, FacePicture?> get studentsFaceImage => _studentsFaceImage;
+
+  static int _sortLessonsByDateTime(Lesson a, Lesson b) {
+    return a.utcDateTime.compareTo(b.utcDateTime);
+  }
+
+  static int _sortAttendancesByDateTime(Attendance a, Attendance b) {
+    return a.lesson.utcDateTime.compareTo(b.lesson.utcDateTime);
+  }
 
   Map<Student, FacePicture?> getStudentFaceImage() {
-    final students = domainRepository
-        .getStudentFromSubjectClass([subjectClass])[subjectClass]!;
-    return domainRepository.getFacePictureFromStudent(students);
+    final students = _domainRepository
+        .getStudentFromSubjectClass([_subjectClass])[_subjectClass]!;
+    return _domainRepository.getFacePictureFromStudent(students);
   }
 
   Map<Student, List<Attendance>> getSubjectClassAttendance() {
-    return domainRepository
-        .getSubjectClassAttendance([subjectClass])[subjectClass] ?? Map.unmodifiable({});
+    return _domainRepository
+        .getSubjectClassAttendance([_subjectClass])[_subjectClass] ?? {};
   }
+
 }
