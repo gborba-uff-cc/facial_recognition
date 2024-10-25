a3193420d08ed291e282141db4c4d87fb590eee9
Gabriel Borba <gborba@id.uff.br>
2024-10-25 10:56:08 -0300

    added function to map a 2d coord to a 1d coord on utils/algorithms.dart

--- lib/utils/algorithms.dart
+++ lib/utils/algorithms.dart
@@ -1,5 +1,59 @@
 import 'dart:typed_data';
 
+// ignore: slash_for_doc_comments
+/**
+Map 2D coordinates to 1D coordinates.
+
+Narrows 2D coordinates inside an area with size [xGroupLength] by [yGroupLength]
+as the same 1D coordinate inside a group of size [length]
+
+for a 2D matrix with width=6, heigth=4 and with an input of xGroupLength=2,
+yGroupLength=2, xLength=8, length=2 this problem instance can be seem as:
+```text
+       0 1 2 3 4 5
+       | | | | | |
+  0 - A A B B C C
+  1 - A A B B C C  -->  0 1 2 3 4 5 6 7 8 9 101112131415
+  2 - D D E E F F  -->  A * B * C * * * D * E * F * * *
+  3 - D D E E F F
+```
+
+where:
+```text
+  A A
+  A A
+```
+is [xGroupLength] by [yGroupLength] in size
+
+```text
+  A *
+```
+is [length] in size
+
+```text
+  A * B * C * * *
+```
+is [xLength] in size and dictate the 1D index for the first 2D group on the
+second line of groups
+
+result in:
+```text
+  {(0,0), (0,1), (1,0), (1,1)} -> 0
+  {(0,4), (0,5), (1,4), (1,5)} -> 4
+  {(2,0), (2,1), (3,0), (3,1)} -> 8
+```
+*/
+int map2dTo1dCoordinate(
+  final int x,
+  final int y, {
+  final int xGroupLength = 1,
+  final int yGroupLength = 1,
+  required final int xLength,
+  final int length = 1,
+}) {
+  return x ~/ xGroupLength * length + y ~/ yGroupLength * xLength;
+}
+
 bool areIterablesEquivalents<T>(
   Iterable<T> iterable1,
   Iterable<T> iterable2,
